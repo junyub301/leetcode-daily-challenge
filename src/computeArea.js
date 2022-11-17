d6d11cdd110999ef78b9e2d491f5f695ee6f6b88
junyub301 <junyub0301@gmail.com>
2022-11-17 16:39:19 +0900

    2022/11/17 Rectangle Area

--- src/computeArea.js
+++ src/computeArea.js
@@ -9,6 +9,8 @@
  * @param {number} by2
  * @return {number}
  */
+
+// 내가한 풀이
 var computeArea = (ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) => {
     let aArea = (ax2 - ax1) * (ay2 - ay1);
     let bArea = (bx2 - bx1) * (by2 - by1);
@@ -22,13 +24,32 @@ var computeArea = (ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) => {
     }
 };
 
-var computeAreaIsSolution = () => {};
+// 솔루션에서 제공해주는 풀이
+var computeAreaIsSolution = () => {
+    const areaOfA = (ay2 - ay1) * (ax2 - ax1);
+    const areaOfB = (by2 - by1) * (bx2 - bx1);
 
-//17
-console.log(computeArea(-2, -2, 2, 2, -4, 3, -3, 4));
-//45
-console.log(computeArea(-3, 0, 3, 4, 0, -1, 9, 2));
-//4
-console.log(computeArea(0, 0, 0, 0, -1, -1, 1, 1));
-//17
-console.log(computeArea(-2, -2, 2, 2, 3, 3, 4, 4));
+    // calculate x overlap
+    const left = Math.max(ax1, bx1);
+    const right = Math.min(ax2, bx2);
+    const xOverlap = right - left;
+
+    // calculate y overlap
+    const top = Math.min(ay2, by2);
+    const bottom = Math.max(ay1, by1);
+    const yOverlap = top - bottom;
+
+    let areaOfOverlap = 0;
+    // if the rectangles overlap each other, then calculate
+    // the area of the overlap
+    if (xOverlap > 0 && yOverlap > 0) {
+        areaOfOverlap = xOverlap * yOverlap;
+    }
+
+    // areaOfOverlap is counted twice when in the summation of
+    // areaOfA and areaOfB, so we need to subtract it from the
+    // total, to get the toal area covered by both the rectangles
+    const totalArea = areaOfA + areaOfB - areaOfOverlap;
+
+    return totalArea;
+};
