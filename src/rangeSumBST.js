a9284083975eeb3615ff500fea3c0a89c969de94
junyub301 <junyub0301@gmail.com>
2022-12-07 15:36:15 +0900

    2022/12/07 Range Sum of BST

---
+++ src/rangeSumBST.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+    let sum = 0;
+    const dfs = (node) => {
+        if (node.val >= low && node.val <= high) {
+            sum += node.val;
+        }
+        if (node.left) dfs(node.left);
+        if (node.right) dfs(node.right);
+        return node;
+    };
+    dfs(root);
+
+    return sum;
+};
