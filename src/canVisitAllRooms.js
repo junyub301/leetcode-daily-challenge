487e947020b2aa9ed85fdbfdf03dc7ab6473ba98
junyub301 <junyub0301@gmail.com>
2022-12-20 16:13:31 +0900

    2022/12/20  Keys and Rooms

---
+++ src/canVisitAllRooms.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function (rooms) {
+    let visited = Array.from({ length: rooms.length }, () => false);
+    visited[0] = true;
+    let stack = [0];
+    while (stack.length) {
+        let node = stack.pop();
+        for (let val of rooms[node]) {
+            if (!visited[val]) {
+                visited[val] = true;
+                stack.push(val);
+            }
+        }
+    }
+    console.log(visited);
+    return visited.every((v) => v === true);
+};
+
+console.log(canVisitAllRooms([[1, 3], [3, 0, 1], [2], [0]]));
