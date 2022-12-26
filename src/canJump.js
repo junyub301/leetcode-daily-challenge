e9b5bf3e46bbbec63c63072b67320deab5d81364
junyub301 <junyub0301@gmail.com>
2022-12-26 16:20:58 +0900

    2022/12/26 Jump Game

---
+++ src/canJump.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var canJump = function (nums) {
+    let max = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > max) return false;
+        max = Math.max(max, i + nums[i]);
+    }
+    return true;
+};
+
+console.log(canJump([3, 2, 1, 0, 4]));
