c54940c2b91fd711982303097a364bda169a72d1
junyub301 <junyub0301@gmail.com>
2023-01-17 17:25:38 +0900

    2023/1/17  Flip String to Monotone Increasing

--- src/minFlipsMonoIncr.js
+++ src/minFlipsMonoIncr.js
@@ -8,6 +8,8 @@ var minFlipsMonoIncr = function (s) {
 
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "0") {
+            // ans + 1 =>  '0'을 '1'로 뒤집기
+            // num => 이전의 '1'을 다 '0'으로 뒤집기
             ans = Math.min(num, ans + 1);
         } else {
             num++;
