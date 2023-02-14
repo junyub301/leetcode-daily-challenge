2c0e69ed0b1121c23911a517ad567b3796baa3fa
junyub301 <junyub0301@gmail.com>
2023-02-14 16:45:19 +0900

    2023/2/14 Add Binary

---
+++ src/countOdds.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+    let cnt = 0;
+    for (let i = low; i <= high; i++) {
+        if (i % 2 !== 0) cnt++;
+    }
+
+    return cnt;
+};
+
+let low = 8,
+    high = 10;
+console.log(countOdds(low, high));
