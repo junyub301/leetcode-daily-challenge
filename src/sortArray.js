376bedcedf95ab4fceea656f2ac17b82a1419d8f
junyub301 <junyub0301@gmail.com>
2023-03-13 16:00:59 +0900

    2023/3/1 Sort an Array

---
+++ src/sortArray.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+function merge(arr1, arr2) {
+    const result = [];
+    let i = 0,
+        j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+}
+
+var sortArray = function (nums) {
+    if (nums.length <= 1) return nums;
+    let mid = Math.floor(nums.length / 2);
+    let left = sortArray(nums.slice(0, mid));
+    let right = sortArray(nums.slice(mid));
+    return merge(left, right);
+};
+
+const nums = [5, 2, 3, 1];
+console.log(sortArray(nums));
